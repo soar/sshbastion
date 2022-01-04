cd0c03f8a0940b2332e6e0934576626c91557dea
Aleksey @soar Smyrnov <i@soar.name>
2022-01-04 21:41:13 +0300

    Add `COPY_KEYS` option

--- rootfs/usr/local/bin/sshbastion.sh
+++ rootfs/usr/local/bin/sshbastion.sh
@@ -9,13 +9,23 @@ for host_key_type in ${HOST_KEY_TYPES}; do
 
     if [ -r "${SECRETSDIR}/${keyfilename}" ]; then
         echo "Precreated key ${keyfilename} found, will be used as host key..."
-        ln -s "${SECRETSDIR}/${keyfilename}" "${HOST_KEYS_DIR}/${keyfilename}"
+        if [ "$COPY_KEYS" ]; then
+            cp "${SECRETSDIR}/${keyfilename}" "${HOST_KEYS_DIR}/${keyfilename}"
+            chmod 0400 "${HOST_KEYS_DIR}/${keyfilename}"
+        else
+            ln -s "${SECRETSDIR}/${keyfilename}" "${HOST_KEYS_DIR}/${keyfilename}"
+        fi
         ssh-keygen -y -f "${HOST_KEYS_DIR}/${keyfilename}" > "${HOST_KEYS_DIR}/${keyfilename}.pub"
     else
         echo "Precreated key ${keyfilename} not found, host key will be generated..."
         ssh-keygen -A
     fi
 done
+
+if [ "$COPY_KEYS" ]; then
+    cp "${SECRETSDIR}/authorized_keys" "${BASTION_HOMEDIR}/.ssh/authorized_keys"
+    chmod u=rw,go=r "${BASTION_HOMEDIR}/.ssh/authorized_keys"
+fi
 
 echo "Dockerizing..."
 dockerize -template /etc/ssh/sshd_config.gotpl:/etc/ssh/sshd_config
