d8ab0a959207d58ad41905ffca2f33248402a1af
Aleksey @soar Smyrnov <i@soar.name>
2022-01-04 22:26:59 +0300

    Fix startup script

--- rootfs/usr/local/bin/sshbastion.sh
+++ rootfs/usr/local/bin/sshbastion.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 
-HOST_KEY_TYPES="dsa ed25519 ecdsa rsa"
-SECRETSDIR=/run/secrets
-HOST_KEYS_DIR=/etc/ssh
+: "${HOST_KEY_TYPES:-"dsa ed25519 ecdsa rsa"}"
+: "${SECRETSDIR:-/run/secrets}"
+: "${HOST_KEYS_DIR:-/etc/ssh}"
 
 for host_key_type in ${HOST_KEY_TYPES}; do
     keyfilename="ssh_host_${host_key_type}_key"
@@ -18,9 +18,9 @@ for host_key_type in ${HOST_KEY_TYPES}; do
         ssh-keygen -y -f "${HOST_KEYS_DIR}/${keyfilename}" > "${HOST_KEYS_DIR}/${keyfilename}.pub"
     else
         echo "Precreated key ${keyfilename} not found, host key will be generated..."
-        ssh-keygen -A
     fi
 done
+ssh-keygen -A
 
 if [ "$COPY_KEYS" ]; then
     cp "${SECRETSDIR}/authorized_keys" "${BASTION_HOMEDIR}/.ssh/authorized_keys"
@@ -31,4 +31,4 @@ echo "Dockerizing..."
 dockerize -template /etc/ssh/sshd_config.gotpl:/etc/ssh/sshd_config
 
 echo "Starting sshd..."
-/usr/sbin/sshd -D -e "$@"
+exec /usr/sbin/sshd -D -e "$@"
